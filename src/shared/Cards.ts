60dc70ece11a8daaea76a6a2cfe5b1e4529024f7
milad sadeghi <miladsadeghi2323@gmail.com>
2024-02-17 16:54:13 +0330

    chore: Update cards content

--- src/shared/Cards.ts
+++ src/shared/Cards.ts
@@ -1,44 +1,44 @@
 const Cards: IKanbanInfo[] = [
   // BACKLOG
   {
-    title: 'Look into render bug in dashboard',
+    title: 'Investigate dashboard rendering issue',
     id: '1',
     column: 'backlog',
     bgColor: 'bg-[#DA3A3A]',
   },
   {
-    title: 'SOX compliance checklist',
+    title: 'Compliance checklist for SOX',
     id: '2',
     column: 'backlog',
     bgColor: 'bg-[#C94AA8]',
   },
   {
-    title: '[SPIKE] Migrate to Azure',
+    title: 'Potential migration to Azure',
     id: '3',
     column: 'backlog',
     bgColor: 'bg-[#7678D1]',
   },
   {
-    title: 'Document Notifications service',
+    title: 'Documentation for Notifications service',
     id: '4',
     column: 'backlog',
     bgColor: 'bg-[#7678D1]',
   },
   // TODO
   {
-    title: 'Research DB options for new microservice',
+    title: 'Explore database options for new microservice',
     id: '5',
     column: 'todo',
     bgColor: 'bg-[#00B294]',
   },
   {
-    title: 'Postmortem for outage',
+    title: 'Analyze outage and create postmortem',
     id: '6',
     column: 'todo',
     bgColor: 'bg-[#7678D1]',
   },
   {
-    title: 'Sync with product on Q3 roadmap',
+    title: 'Schedule meeting with product about Q3 roadmap',
     id: '7',
     column: 'todo',
     bgColor: 'bg-[#00B294]',
@@ -46,20 +46,20 @@ const Cards: IKanbanInfo[] = [
 
   // DOING
   {
-    title: 'Refactor context providers to use Zustand',
+    title: 'Update context providers to use Zustand',
     id: '8',
     column: 'doing',
     bgColor: 'bg-[#3F88E4]',
   },
   {
-    title: 'Add logging to daily CRON',
+    title: 'Implement logging for daily CRON task',
     id: '9',
     column: 'doing',
     bgColor: 'bg-[#7678D1]',
   },
   // DONE
   {
-    title: 'Set up DD dashboards for Lambda listener',
+    title: 'Configure DD dashboards for Lambda listener)',
     id: '10',
     column: 'done',
     bgColor: 'bg-[#DA3A3A]',
